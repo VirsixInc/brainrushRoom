0aaa7c6ba1cf1817b4b69969e74dd20616ca155a
Matthew Sauer <matt@virsix.com>
2015-02-03 11:09:20 -0800

    trying to get animations working

---
+++ Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimation : MonoBehaviour {
+
+	public Animator myAnimator;
+	public GameObject activationObject;
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = gameObject.GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(activationObject != null){
+			playAnimation(activationObject);
+		}
+
+	}
+
+	void playAnimation(GameObject activator){
+
+		if(activator.activeSelf){
+			print ("dicks");
+			myAnimator.SetBool("isOpen", true);
+		}
+	}
+}
