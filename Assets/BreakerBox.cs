e50378857735bae6d015b877d1e744e69db3a99f
Pablo Leon-Luna <paleonluna@gmail.com>
2015-02-03 07:08:02 -0800

    I am in love with a stripper

---
+++ Assets/BreakerBox.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakerBox : MonoBehaviour {
+
+	public Light[] overheadLights;
+	public Animator breakerSwitch;
+	void OnMouseDown() {
+		breakerSwitch.SetTrigger ("pull");
+	}
+}
