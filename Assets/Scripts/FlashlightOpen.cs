5c0a80fa978ecd06199733a7da223848e3e60a69
Pablo Leon-Luna <paleonluna@gmail.com>
2015-02-04 11:05:10 -0800

    my homies

--- Assets/Scripts/FlashlightOpen.cs
+++ Assets/Scripts/FlashlightOpen.cs
@@ -3,25 +3,21 @@ using System.Collections;
 
 public class FlashlightOpen : MonoBehaviour {
 	Animator myAnimator;
-	BoxCollider myCollider;
 	public BoxCollider frontCollider;
 	public BoxCollider backCollider;
-	public GameObject light;
+	public GameObject myLight;
 	public GameObject battery;
 	public bool isBuilt = false;
 	bool lightOn = false;
 	// Use this for initialization
 	void Start () {
 		myAnimator = gameObject.GetComponent<Animator>();
-		myCollider = gameObject.GetComponent<BoxCollider>();
 
 	}
 
 	void PickUpFlashlight() {
-		//print (isBuilt.ToString () + "     " + lightOn.ToString ());
 		if(isBuilt && !lightOn){
 			Camera.main.transform.GetChild(0).gameObject.SetActive(true);
-			Debug.Log ("lightOn");
 			lightOn = true;
 			gameObject.SetActive(false);
 		}
@@ -29,24 +25,23 @@ public class FlashlightOpen : MonoBehaviour {
 	}
 
 	void OnMouseDown(){
-		if(myAnimator.GetBool("isOpen")){
+		if(myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Open")){
 			frontCollider.enabled = false;
 			backCollider.enabled = false;
-			myAnimator.SetBool("isOpen", false);
+			myAnimator.SetTrigger("switch");
 			if(!isBuilt){
-				print (light.activeSelf.ToString() + "     " + battery.activeSelf.ToString() );
-				if(light.activeSelf == true && battery.activeSelf){
+				if(myLight.activeSelf == true && battery.activeSelf){
 					print(isBuilt.ToString());
 					isBuilt = true;
 				}
 			}
 
 		}
-		else if(!myAnimator.GetBool("isOpen")){
+		else if(!myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Open")){
 
 			frontCollider.enabled = true;
 			backCollider.enabled = true;
-			myAnimator.SetBool("isOpen", true);
+			myAnimator.SetTrigger("switch");
 		}
 	}
 }
--- Assets/Scripts/PuzzleObjectActivator.cs
+++ Assets/Scripts/PuzzleObjectActivator.cs
@@ -5,6 +5,7 @@ public class PuzzleObjectActivator : MonoBehaviour {
 
 	public string key;
 	public string optionalMessage;
+	
 
 	void OnMouseDown() {
 		print ("PUZZLEOBJECTACTIVATOR");
