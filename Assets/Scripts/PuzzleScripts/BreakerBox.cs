b961b22483808e4b5a58444a9411d2aa68ed0d30
Pablo Leon-Luna <paleonluna@gmail.com>
2015-02-11 11:26:01 -0800

    fixed everything

--- Assets/Scripts/PuzzleScripts/BreakerBox.cs
+++ Assets/Scripts/PuzzleScripts/BreakerBox.cs
@@ -5,19 +5,23 @@ public class BreakerBox : MonoBehaviour {
 
 	public Light[] overheadLights;
 	public Animator breakerSwitch;
+	public GameObject breakerHandle;	
 	void OnMouseDown() {
-		breakerSwitch.SetTrigger ("pull");
-		StartCoroutine ("TurnOnLights");
+		if (breakerHandle.activeSelf) {
+			breakerSwitch.SetTrigger ("start");
+			StartCoroutine ("TurnOnLights");
+		}
 	}
 
 	IEnumerator TurnOnLights() {
-		while (!breakerSwitch.GetCurrentAnimatorStateInfo(0).IsName("Rotate-70")) {
+		while (!breakerSwitch.GetCurrentAnimatorStateInfo(0).IsName("On")) {
 			yield return new WaitForSeconds(0);
 		}
 		print ("LIGHTS");
 
 		foreach (Light x in overheadLights)
 			x.gameObject.SetActive (true);
+		Camera.main.transform.GetChild (0).gameObject.SetActive (false);
 	}
 
 }
