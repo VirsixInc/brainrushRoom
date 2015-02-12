5fbcf9312e2bbbe14185e2777fc1977cc2085e6b
Pablo Leon-Luna <paleonluna@gmail.com>
2015-02-12 09:15:37 -0800

    adding in stuff

---
+++ Assets/Conveyor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Conveyor : MonoBehaviour {
+
+	bool isTriggerFlipped = false;
+	public GameObject pieceOfMagnet;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+
+	}
+}
