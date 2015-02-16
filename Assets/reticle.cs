ae285652cd978d5a54dac5b71bde8a82af3841cc
virsixjon <jon@virsix.com>
2015-02-16 13:29:22 -0800

    added reticle to camera

--- Assets/reticle.cs
+++ Assets/reticle.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 public class reticle : MonoBehaviour {
 	public GameObject retPrefab;
-	public float clickX;
-	public float clickY;
-	public Vector3 camRay;
+	float clickX;
+	float clickY;
+	Vector3 camRay;
 	public float zPos;
 
 	// Use this for initialization
 	void Start () {
-
 	}
 	// Update is called once per frame
 	void Update () {
@@ -19,7 +18,7 @@ public class reticle : MonoBehaviour {
 			clickY = Input.mousePosition.y;
 			camRay = camera.ScreenToWorldPoint (new Vector3(clickX,clickY,zPos));
 			GameObject obj = (GameObject) Instantiate(retPrefab, camRay, Quaternion.identity);
-			Destroy(obj, obj.animation.clip.length);
+
 		}
 
 	
--- Assets/reticleAnim.cs
+++ Assets/reticleAnim.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class reticleAnim : MonoBehaviour {
 	public float Time;
+	GameObject player;
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.FindGameObjectWithTag ("MainCamera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-			Destroy (gameObject, Time);
+		transform.SetParent (player.transform);
+		Destroy (gameObject, Time);
 	}
 
 
