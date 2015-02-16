0dd5abb0b5016e806aacdc1acea6fc35464216bd
virsixjon <jon@virsix.com>
2015-02-16 12:52:43 -0800

    made reticle script and tweaked reticle prefab

---
+++ Assets/reticle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reticle : MonoBehaviour {
+	public GameObject retPrefab;
+	public float clickX;
+	public float clickY;
+	public Vector3 camRay;
+	public float zPos;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			clickX = Input.mousePosition.x;
+			clickY = Input.mousePosition.y;
+			camRay = camera.ScreenToWorldPoint (new Vector3(clickX,clickY,zPos));
+			GameObject obj = (GameObject) Instantiate(retPrefab, camRay, Quaternion.identity);
+			Destroy(obj, obj.animation.clip.length);
+		}
+
+	
+	}
+}
---
+++ Assets/reticleAnim.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reticleAnim : MonoBehaviour {
+	public float Time;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			Destroy (gameObject, Time);
+	}
+
+
+
+//	public void animState(){
+//		Destroy (this.gameObject);
+//
+//		}
+
+}
+
+
