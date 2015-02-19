db15496c5c285d3bfa8ab6bf37ec573fd573cc5e
virsixjon <jon@virsix.com>
2015-02-19 15:03:07 -0800

    more room

--- Assets/Mike/LightFlicker.cs
+++ Assets/Mike/LightFlicker.cs
@@ -3,35 +3,29 @@ using System.Collections;
 
 public class LightFlicker : MonoBehaviour {
 
-	public float fullIntensity = 1f;
-	public float flickerPeriod = 1f;
-	public float steadyPeriod = 2f;
-	public int randomSeed = 128;
-	private System.Random randomizer;
-	private float offset = 0f;
 
+	public float offset;
 	// Use this for initialization
 	void Start () {
-	
-		randomizer = new System.Random(randomSeed);
-		offset = randomizer.Next((System.Int32)steadyPeriod * 15);
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		float currentIntensity = 0f;
-		float cycleTime = Time.time + offset;
-		currentIntensity = Mathf.Max (Mathf.Ceil(Mathf.Sin(cycleTime/steadyPeriod)),Mathf.Ceil(Mathf.Sin (cycleTime /(2 * steadyPeriod * 0.333333F))));
-		//irregular strobe
-		light.intensity = Mathf.Max (currentIntensity, flicker (cycleTime/flickerPeriod)) * fullIntensity;
-	}
+		offset = Random.Range (1f, 100f);
+		foreach (GameObject obj in GameObject.FindGameObjectsWithTag("light")) {
+		
+			if(offset >99){
+				obj.light.intensity = Random.Range (0.3f,.5f);
+			}
+			else
+				{
+					obj.light.intensity = 1;
+				}
 
-	private float flicker (float flickerIn)
-	{
-		flickerIn = flickerIn - Mathf.Floor (flickerIn);
-		flickerIn = 0.5f * (1f + (Mathf.Sin (3.0f / flickerIn) * Mathf.Sin (5f / (1 - flickerIn))));
-		return flickerIn;
+		}
+	
 	}
 }
+
