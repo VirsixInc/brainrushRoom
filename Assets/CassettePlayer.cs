dd6362c621908aed9c3da675d0ef5973225a67b0
Pablo Leon-Luna <paleonluna@gmail.com>
2015-02-20 05:57:36 -0800

    cassette

---
+++ Assets/CassettePlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CassettePlayer : MonoBehaviour {
+	public GameObject tape;
+	// Use this for initialization
+	void OnClick () {
+		if (tape.activeSelf) {
+			GetComponent<Animator> ().SetTrigger ("start");
+			AudioManager.s_instance.PlayAudioSource("music1");
+			AudioManager.s_instance.PlayAudioSource("loadUp");
+		}
+
+	}
+}
