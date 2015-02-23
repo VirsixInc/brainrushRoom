1886ac2416378f49dfd278700821afc61ce13d09
Pablo Leon-Luna <paleonluna@gmail.com>
2015-02-23 10:41:46 -0800

    fixed android input

--- Assets/Scripts/reticleAnim.cs
+++ Assets/Scripts/reticleAnim.cs
@@ -3,23 +3,11 @@ using System.Collections;
 
 public class reticleAnim : MonoBehaviour {
 	public float Time;
-	GameObject player;
-	// Use this for initialization
-	void Start () {
-		player = GameObject.FindGameObjectWithTag ("MainCamera");
-	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		Destroy (gameObject, Time);
 	}
 
-
-
-//	public void animState(){
-//		Destroy (this.gameObject);
-//
-//		}
 
 }
 
