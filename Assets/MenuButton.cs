320211720f2da7676d79b5168ee7294b0f44a9f9
Pablo Leon-Luna <paleonluna@gmail.com>
2015-03-04 13:34:35 -0800

    menu in place

---
+++ Assets/MenuButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour {
+
+	public GameObject inGameMenu;
+
+	void Start(){
+
+	}
+
+	public void OnClick() {
+		if (inGameMenu.activeSelf == false) {
+			inGameMenu.SetActive (true);
+			GameManager.s_instance.currentGameState = GameState.Menu;
+		}
+		else {
+			inGameMenu.SetActive (false);
+			GameManager.s_instance.currentGameState = GameState.Playing;
+
+		}
+	}
+}
