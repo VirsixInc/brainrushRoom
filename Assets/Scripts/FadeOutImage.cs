fc339ea29169f4d295badce08dd0da90be6d27af
Pablo Leon-Luna <paleonluna@gmail.com>
2015-03-03 18:48:53 -0800

    intro scene

---
+++ Assets/Scripts/FadeOutImage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeOutImage : MonoBehaviour {
+	
+	public bool fadeOut;
+	public float startTime, fadeTime = 3;
+	public Image thisImage;
+	
+	void Update() {
+		if (fadeOut) {
+			float timePassed = (Time.time - startTime);
+			float fracJourney = timePassed / fadeTime;
+			thisImage.color = Color.Lerp (new Color (1f, 1f, 1f, .75f), new Color (1f, 1f, 1f, 0f), fracJourney);
+		}
+	}
+	
+	public void StartFade() {
+		startTime = Time.time;
+		fadeOut = true;
+	}
+}
