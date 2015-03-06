464efa93a29e250282cea957eddcb92eb211b583
Pablo Leon-Luna <paleonluna@gmail.com>
2015-03-06 08:54:37 -0800

    particles

--- Assets/Scripts/PuzzleScripts/MagnetRotator.cs
+++ Assets/Scripts/PuzzleScripts/MagnetRotator.cs
@@ -6,12 +6,18 @@ public class MagnetRotator : MonoBehaviour {
 	
 	public bool bothRotating = false;
 	public Light bulb;
-	public GameObject bulbToBePlaced;
+	public GameObject bulbToBePlaced, particle1, particle2;
 	public Animator leverA, leverB;
 //	public GameObject mag1,mag2,mag3,mag4;
 
 	// Update is called once per frame
 	void Update () {
+
+		if (leverA.GetCurrentAnimatorStateInfo (0).IsName ("lever2"))
+						particle1.SetActive (true);
+		if (leverB.GetCurrentAnimatorStateInfo (0).IsName ("lever2"))
+			particle1.SetActive (true);
+
 
 		if (leverA.GetCurrentAnimatorStateInfo (0).IsName ("lever2") && leverB.GetCurrentAnimatorStateInfo (0).IsName ("lever2")) {
 			bothRotating = true;	
